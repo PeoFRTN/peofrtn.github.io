7db312d102be511c714386b63f1424ae26df268e
Peo FRTN <pierre-olivier.fortin@hotmail.com>
2019-08-06 22:32:01 -0400

    MethodsFees

--- js/main.js
+++ js/main.js
@@ -24,13 +24,15 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 directionsDisplay.setMap(map);
 
 // WEEKDAYS OR WEEKENDS ?
-var today = new Date();
-if(today.getDay() == 6 || today.getDay() == 0) alert('Weekend rates') 
-else 
-    alert('Weekdays rate');
+// var today = new Date();
+// if(today.getDay() == 6 || today.getDay() == 0) alert('Weekend rates') 
+// else 
+//   alert('Weekdays rate');
 // *TODO: ADD THE REST OF THE FUNCTION. WEEKDAYS FORMULA FOR WEEKDAYS AND WEEKEND FORMULA FOR WEEKEND
 // IF (OUTPUT WEEKDAYS FEES) ELSE (OUTPUT WEEKENDS FEES)
 
+
+//REMOVE COMMENTS TO ENABLE LOCATION-1
 //document.getElementById("location-1").value,
 // Define calcRoute function
 function calcRoute() {
@@ -46,21 +48,50 @@ function calcRoute() {
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
+
             //Get distance and time            
             var dist = result.routes[0].legs[0].distance.text;
             var dura = result.routes[0].legs[0].duration.text;
+
+
             //Variables needed (distance and duration, the function makes sure its a float(1.11) and not an integer(1)) 
             var distFloat = parseFloat(dist);
             var duraFloat = parseFloat(dura);
+
+
             //Formula for the delivery fees  
             var fees = ( ((duraFloat + 5)/2) + (distFloat) );
-            //The delivery is rounded to the nearest 2.5 using the function ceiling(created at the bottom of this sheet)
-            var feesceil = ceiling(fees, 2.5);
+
+
+            //The delivery is rounded to the nearest 1.0 using the function ceiling(created at the bottom of this sheet)
+            var feesceil = ceiling(fees, 1.0);
+
+            // OUTPUT 2 FORMULA (BASED ON HOURLY WAGE AND KM ALLOWANCE)
+            var hourlyWage = 20;
+            var kmAllowance = 0.50;
+            var fees2 = ((((2*duraFloat)+5)*(hourlyWage/60))+(2*distFloat*kmAllowance));
+            var feesceil2 = ceiling(fees2, 1.0);
+
+            // OUTPUT 3 FORMULA (BASE FEE, UBER METHOD)
+            var pickupFee = 2.50 ;
+            var dropoffFee = 3.00 ;
+            var kmFees = distFloat*kmAllowance;
+            // var KmAllowance borrowed from Output 2
+            var fees3 = (pickupFee + dropoffFee)+ (kmFees);
+            var feesceil3 = ceiling(fees3, 1.0);
+
             //Output line. to display an extra variable
             // simpy add the info in this manner(just before the </div> tag )
             //  .<br />VARIABLE NAME(DISPLAY ON SCREEN): " + var + "      /// where var is replaced by your var name
-            $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".<br />Fees: " + feesceil +"</div>");
+            $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".<br />Fees (Simple): " + feesceil +" <br> PLEASE ONLY USE THE FEES(SIMPLE) FOR NOW, OTHERS ARE IN BETA<br></div>");
             document.getElementById("output").style.display = "block";
+
+
+            $("#output2").html("<div class='result-table'> Fees (Complex): " + feesceil2 + " <br> (2 * Duration + 5 minutes )(hourly Wage / 60 minutes) + (2 * Distance(km) * Km Allowance)</div>");
+            document.getElementById("output2").style.display = "block";
+
+            $("#output3").html("<div class='result-table'> Fees (Uber Method): " + feesceil3 + " <br> This method includes a pick up fee (2.50), a dropoff fee (3.00) and the km fee (Distance(kms) * Kms Allowance(in this case: " + kmFees +"))</div>");
+            document.getElementById("output3").style.display = "block";
 
             //display route
             directionsDisplay.setDirections(result);
@@ -72,7 +103,7 @@ function calcRoute() {
 
             //Show error message           
            
-            alert("Can't find road! Please try again!");
+            alert("Can't find the address! Please try again!");
             clearRoute();
         }
     });
